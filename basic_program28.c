a4b04d4473401130787e21cdf725adb39cb1e6b1
SAGAR T <ssagu6661@gmail.com>
2024-10-09 06:46:31 +0530

    Add files via upload

---
+++ basic_program28.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main(){
+  int sum=0;
+  for (int i = 0; i <= 100; )
+  {
+sum +=i;
+  }
+    printf("Sum of from numbers 1 to 100 is: %d\n",sum);
+
+}
+
+
+
+
+// #include<stdio.h>
+// void main(){
+//   int sum=0;
+//   int i=0;
+//   for (int i = 0; i <= 100; )
+//   {
+// i++;
+  
+//     printf("Sum of from numbers 1 to 100 is: %d\n", i);
+//   }
+// }
